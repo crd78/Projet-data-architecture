725b74e1f8497cfc04ee4de1c7619033bfe36d5a
tristan_retali <tristan.retali@gmail.com>
2026-04-09 09:53:55 +0200

    Initilisation de la partie front

---
+++ Application/Front/urban-data-explorer/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+return (
+<main className="min-h-screen flex items-center justify-center bg-slate-100">
+  <h1 className="text-4xl font-bold text-blue-600">
+    Tailwind fonctionne
+  </h1>
+</main>
+);
+}
+
+export default App;
