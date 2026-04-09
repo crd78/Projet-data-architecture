042910ddf7ee9b678b251d141234be0cdaaf683a
tristan_retali <tristan.retali@gmail.com>
2026-04-09 14:42:59 +0200

    Impossible de bouger la carte

--- Application/Front/urban-data-explorer/src/components/map/MapView.jsx
+++ Application/Front/urban-data-explorer/src/components/map/MapView.jsx
@@ -8,8 +8,6 @@ const INITIAL_VIEW_STATE = {
     longitude: 2.3522,
     latitude: 48.8566,
     zoom: 11,
-    pitch: 0,
-    bearing: 0
 };
 
 
@@ -23,11 +21,19 @@ function MapView({ geoJsonUrl }) {
     }, [data]);
 
     return (
-        <section className="relative isolate h-[75vh] w-full overflow-hidden rounded-2xl border border-slate-200 bg-slate-200 shadow-sm">
+        <section className="relative isolate h-[85vh] w-full overflow-hidden rounded-2xl border border-slate-200 bg-slate-200 shadow-sm">
             <DeckGL
             viewState={viewState}
             onViewStateChange={({ viewState: next }) => setViewState(next)}
-            controller
+            controller={{
+                dragPan: false,
+                dragRotate: false,
+                touchRotate: false,
+                scrollZoom: false,
+                doubleClickZoom: false,
+                touchZoom: false,
+                keyboard: false
+            }}
             layers={layers}
             style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
             getTooltip={({ object }) => {
