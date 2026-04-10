e307a3a4a9bbf08cf208ff5cd2b16766ed2d79e3
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2026-04-10 14:36:28 +0200

    création automatisation dagster

--- Pipeline/src/definition.py
+++ Pipeline/src/definition.py
@@ -1,9 +1,22 @@
-from dagster import Definitions
+from dagster import Definitions, ScheduleDefinition
 from Bronze.Ingestion import all_ingestion_assets, ingestion_job
 from Silver.preprocessing import all_preprocessing_assets, preprocessing_job
+
+weekly_ingestion_schedule = ScheduleDefinition(
+    job=ingestion_job,
+    cron_schedule="0 2 * * 1",
+    execution_timezone="Europe/Paris",
+)
+
+weekly_preprocessing_schedule = ScheduleDefinition(
+    job=preprocessing_job,
+    cron_schedule="30 2 * * 1",
+    execution_timezone="Europe/Paris",
+)
 
 defs = Definitions(
     assets=all_ingestion_assets + all_preprocessing_assets,
     jobs=[job for job in [ingestion_job, preprocessing_job] if job is not None],
+    schedules=[weekly_ingestion_schedule, weekly_preprocessing_schedule],
 )
 
